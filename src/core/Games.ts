c66f62fbffdd48e390b274598d7c14dfb63035cb
xu-hongyi <13103682353@163.com>
2020-06-18 08:55:07 +0800

    触底判定，消除

--- src/core/Games.ts
+++ src/core/Games.ts
@@ -3,94 +3,113 @@ import SquareGroup from "@/core/SquareGroup";
 import {createTetris} from "@/core/Tetris";
 import {TetrisRules} from "@/core/TetrisRule";
 import gameConfig from "@/core/gameConfig";
+import {Square} from "@/core/Square";
 
 export class Games {
     //游戏状态
-    private _gameStatus:GameStatus = GameStatus.init
+    private _gameStatus: GameStatus = GameStatus.init
     //下一个方块类型
-    private _nextTetris:SquareGroup = createTetris({x:0, y:0})
+    private _nextTetris: SquareGroup = createTetris({x: 0, y: 0})
     //计时器，控制方块下落
-    private _timer?:number
+    private _timer?: number
     //控制方块下落时间
-    private _duration:number = 1000
+    private _duration: number = 1000
     //当前控制的小方块
-    private _curTetris?:SquareGroup
+    private _curTetris?: SquareGroup
+    //存储下落后存在的方块
+    private _existArr:Square[] = []
     constructor(
-        private _viewer:GameViewer
+        private _viewer: GameViewer
     ) {
         this._viewer.showNext(this._nextTetris)
         this.resetCenterPoint(gameConfig.nextSize.width, this._nextTetris)
     }
+
     //游戏开始
-    start(){
-        if(this._gameStatus === GameStatus.playing) return;
+    start() {
+        if (this._gameStatus === GameStatus.playing) return;
         this._gameStatus = GameStatus.playing
-        if(!this._curTetris){
+        if (!this._curTetris) {
             this.switchTetris()
         }
         this.autoDrop()
     }
+
     //控制方块自由下落
-    private autoDrop(){
-        if(this._timer || this._gameStatus !== GameStatus.playing) return;
-        this._timer = setInterval(() =>{
+    private autoDrop() {
+        if (this._timer || this._gameStatus !== GameStatus.playing) return;
+        this._timer = setInterval(() => {
             if (this._curTetris) {
-                TetrisRules.move(this._curTetris, MoveDirection.down)
+                if(!TetrisRules.move(this._curTetris, MoveDirection.down, this._existArr)){
+                    this.hitBottom()
+                }
             }
         }, this._duration)
     }
+
     //切换方块
-    private switchTetris(){
+    private switchTetris() {
         this._curTetris = this._nextTetris
         this.resetCenterPoint(gameConfig.panelSize.width, this._curTetris)
-        this._nextTetris = createTetris({x:0, y:0})
+        this._nextTetris = createTetris({x: 0, y: 0})
         this.resetCenterPoint(gameConfig.nextSize.width, this._nextTetris)
         this._viewer.switch(this._curTetris)
         this._viewer.showNext(this._nextTetris)
     }
 
-    private resetCenterPoint = (width:number, tetris:SquareGroup) => {
+    private resetCenterPoint = (width: number, tetris: SquareGroup) => {
         const x = Math.ceil(width / 2) - 1
         const y = 0
         tetris.centerPoint = {x, y}
-        while (tetris.squares.some(s => s.point.y < 0)){
-            tetris.squares.forEach(sq =>{
+        while (tetris.squares.some(s => s.point.y < 0)) {
+            tetris.squares.forEach(sq => {
                 sq.point = {
-                    x:sq.point.x,
-                    y:sq.point.y + 1
+                    x: sq.point.x,
+                    y: sq.point.y + 1
                 }
             })
         }
     };
 
-    controlDown(){
-        if(this._gameStatus === GameStatus.playing && this._curTetris){
-            TetrisRules.moveDirectly(this._curTetris, MoveDirection.down)
+    controlDown() {
+        if (this._gameStatus === GameStatus.playing && this._curTetris) {
+            if(!TetrisRules.moveDirectly(this._curTetris, MoveDirection.down, this._existArr)){
+                this.hitBottom()
+            }
         }
     }
 
-    controlLeft(){
-        if(this._gameStatus === GameStatus.playing && this._curTetris){
-            TetrisRules.move(this._curTetris, MoveDirection.left)
+    controlLeft() {
+        if (this._gameStatus === GameStatus.playing && this._curTetris) {
+            TetrisRules.move(this._curTetris, MoveDirection.left, this._existArr)
         }
     }
 
-    controlRight(){
-        if(this._gameStatus === GameStatus.playing && this._curTetris){
-            TetrisRules.move(this._curTetris, MoveDirection.right)
+    controlRight() {
+        if (this._gameStatus === GameStatus.playing && this._curTetris) {
+            TetrisRules.move(this._curTetris, MoveDirection.right, this._existArr)
         }
     }
 
-    controlRotate(){
-        if(this._gameStatus === GameStatus.playing && this._curTetris){
-            TetrisRules.rotate(this._curTetris)
+    controlRotate() {
+        if (this._gameStatus === GameStatus.playing && this._curTetris) {
+            TetrisRules.rotate(this._curTetris, this._existArr)
         }
     }
-    pause(){
-        if(this._gameStatus === GameStatus.playing){
+
+    pause() {
+        if (this._gameStatus === GameStatus.playing) {
             this._gameStatus = GameStatus.pause
             clearInterval(this._timer)
             this._timer = undefined
         }
     }
+
+    //触底处理
+    private hitBottom(){
+        this._existArr = [...this._existArr, ...this._curTetris!.squares]
+        TetrisRules.removeLine(this._existArr)
+        this.switchTetris()
+    }
+
 }
--- src/core/TetrisRule.ts
+++ src/core/TetrisRule.ts
@@ -1,72 +1,99 @@
 import {IPoint, MoveDirection, Shape} from "@/core/types";
 import gameConfig from "@/core/gameConfig";
 import SquareGroup from "@/core/SquareGroup";
+import {Square} from "@/core/Square";
 
 function isPoint(obj: any): obj is IPoint {
     return obj.x !== undefined;
 }
 
 export class TetrisRules {
-    static canIMove(shape: Shape, targetPoint: IPoint): boolean {
+    static canIMove(shape: Shape, targetPoint: IPoint, exists: Square[]): boolean {
         const targetSquarePoints: IPoint[] = shape.map(i => ({
             x: i.x + targetPoint.x,
             y: i.y + targetPoint.y
         }))
 
-        const result = targetSquarePoints.some(i => {
+        return !targetSquarePoints.some(i => {
             return i.x < 0 || i.x > gameConfig.panelSize.width - 1 || i.y < 0 || i.y > gameConfig.panelSize.height - 1
-        })
-        return !result;
-
+        }) && !targetSquarePoints.some(p => exists.some(s => p.x === s.point.x && p.y === s.point.y))
     }
 
-    static move(tetris: SquareGroup, targetPointOrDirection: IPoint): boolean;
-    static move(tetris: SquareGroup, targetPointOrDirection: MoveDirection): boolean;
-    static move(tetris: SquareGroup, targetPointOrDirection: IPoint | MoveDirection): boolean {
+    static move(tetris: SquareGroup, targetPointOrDirection: IPoint, exists: Square[]): boolean;
+    static move(tetris: SquareGroup, targetPointOrDirection: MoveDirection, exists: Square[]): boolean;
+    static move(tetris: SquareGroup, targetPointOrDirection: IPoint | MoveDirection, exists: Square[]): boolean {
         if (isPoint(targetPointOrDirection)) {
-            if (this.canIMove(tetris.shape, targetPointOrDirection)) {
+            if (this.canIMove(tetris.shape, targetPointOrDirection, exists)) {
                 tetris.centerPoint = targetPointOrDirection
                 return true;
             }
             return false;
         } else {
+            let targetPoint: IPoint;
             if (targetPointOrDirection === MoveDirection.left) {
-                const targetPoint = {
+                targetPoint = {
                     x: tetris.centerPoint.x - 1,
                     y: tetris.centerPoint.y
                 }
-                return this.move(tetris, targetPoint)
             } else if (targetPointOrDirection === MoveDirection.right) {
-                const targetPoint = {
+                targetPoint = {
                     x: tetris.centerPoint.x + 1,
                     y: tetris.centerPoint.y
                 }
-                return this.move(tetris, targetPoint)
-            } else if (targetPointOrDirection === MoveDirection.down) {
-                const targetPoint = {
+            } else {
+                targetPoint = {
                     x: tetris.centerPoint.x,
                     y: tetris.centerPoint.y + 1
                 }
-                return this.move(tetris, targetPoint)
-            } else {
-                return false
             }
+            return this.move(tetris, targetPoint, exists)
         }
     };
 
+    static moveDirectly(tetris: SquareGroup, direction: MoveDirection, exists: Square[]): boolean {
+        while (this.move(tetris, direction, exists)) {
+        }
+        return false
+    }
 
-    static moveDirectly(tetris: SquareGroup, direction: MoveDirection) {
-        while (this.move(tetris, direction)) {
+    static rotate(tetris: SquareGroup, exists: Square[]): boolean {
+        const rotateShape = tetris.afterRotate()
+        if (this.canIMove(rotateShape, tetris.centerPoint, exists)) {
+            tetris.rotate()
+            return true;
+        } else {
+            return false
         }
     }
 
-    static rotate(tetris:SquareGroup):boolean{
-        const rotateShape = tetris.afterRotate()
-        if(this.canIMove(rotateShape, tetris.centerPoint)){
-            tetris.rotate()
-            return true;
-        }else {
-            return false
+    static removeLine(exists:Square[]):number{
+        const ys = exists.map(i => i.point.y)
+        const minY = Math.min(...ys)
+        const maxY = Math.max(...ys)
+        let num = 0
+        for (let i = minY;i <= maxY; i++){
+            const result = this.deleteLine(exists, i)
+            if(result){
+                num ++
+            }
         }
+        return num
+    }
+
+    static deleteLine(exists:Square[], y:number):boolean{
+        const sqs = exists.filter(sq => sq.point.y === y)
+        if(sqs.length === gameConfig.panelSize.width){
+            sqs.forEach(sq => {
+                sq.viewer?.remove()
+                const index = exists.indexOf(sq)
+                exists.splice(index, 1)
+            })
+            exists.filter(sq => sq.point.y < y).forEach(sq => sq.point = {
+                x:sq.point.x,
+                y:sq.point.y + 1
+            })
+            return true
+        }
+        return false
     }
 }
